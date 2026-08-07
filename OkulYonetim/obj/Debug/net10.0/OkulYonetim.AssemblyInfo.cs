98fb1e120ea501a44c2b4eff30088ab4a5d7b13e
tayfunglp7 <tayfunglp7@gmail.com>
2026-08-07 17:35:30 +0300

    commit

--- OkulYonetim/obj/Debug/net10.0/OkulYonetim.AssemblyInfo.cs
+++ OkulYonetim/obj/Debug/net10.0/OkulYonetim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OkulYonetim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd3069ea9f17cbdaa27805bb37a3a9bf9244d849")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d74c4b4c382c97a67765b6d3173dd7743acc4e8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OkulYonetim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OkulYonetim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
